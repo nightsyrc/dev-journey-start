1ab9b76c4a584e1c8c7e4d195dc7bbee5156eb56
Cristian Melendez <cmelendezgp@gmail.com>
2025-04-28 20:37:57 -0600

    Created .clicked class in css, used classList.toggle() in JS.

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const myButton = document.querySelector('button');
 
 // Add a click event
 myButton.addEventListener('click', function() {
-    myButton.style.backgroundColor = '#28a745'; // Change button color (greenish)
-    myButton.style.transform = 'scale(1.2)';    // Slightly grow the button
-    myButton.textContent = 'Clicked! 😘';       // Change button text
+    myButton.classList.toggle('clicked');
   });
