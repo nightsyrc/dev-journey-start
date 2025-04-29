ac2dc7598bbf8b264917718d065923ac2cf27cf8
Cristian Melendez <cmelendezgp@gmail.com>
2025-04-28 20:24:14 -0600

    Added JS func to the button to display a cool message

---
+++ script.js
@@ -0,0 +1,7 @@
+// Find the button
+const myButton = document.querySelector('button');
+
+// Add a click event
+myButton.addEventListener('click', function() {
+  alert('Cristian is unstoppable! 🚀');
+});
